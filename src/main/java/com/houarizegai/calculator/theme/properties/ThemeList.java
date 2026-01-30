bb63e609b1da215ab79e8afa90a7c5c968777334
Rathinam288 <gnanarathinam6@gmail.com>
2026-01-30 10:08:14 +0530

    sonar checking

---
+++ src/main/java/com/houarizegai/calculator/theme/properties/ThemeList.java
@@ -0,0 +1,23 @@
+package com.houarizegai.calculator.theme.properties;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ThemeList {
+    private List<Theme> themes;
+
+    public List<Theme> getThemes() {
+        return themes;
+    }
+
+    public void setThemes(List<Theme> themes) {
+        Objects.requireNonNull(themes, "themes cannot be null");
+        this.themes = themes;
+    }
+
+    public Map<String, Theme> getThemesAsMap() {
+        return themes.stream().collect(Collectors.toMap(Theme::getName, Function.identity()));
+    }
+}
